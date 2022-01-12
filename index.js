9e496211d5b9ef0cd032c55d60561297a4a26e53
Laura Oviedo <lauraoviedosocias@gmail.com>
2022-01-12 16:17:13 -0300

    cambios de clases y ids

---
+++ index.js
@@ -0,0 +1,18 @@
+const charactersButton = document.getElementById("characters-button");
+const episodesButton = document.getElementById("episodes-button");
+const quotesButton = document.getElementById("quotes-button");
+const homeButton = document.getElementById("home-button")
+const menu_hamburguesa = document.getElementById("menu-hamburguesa")
+
+
+const charactersSection = document.getElementById("")
+
+// BASIC FUNCTIONS TO NAVIGATE THE WEB
+
+charactersButton.onclick = () => {
+
+    cards_principales.classList.add("is-hidden");
+    card_reportes.classList.add("is-hidden");
+    card_categorias.classList.remove("is-hidden");
+
+}
