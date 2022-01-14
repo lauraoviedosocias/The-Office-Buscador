2f64603543e2b9dcb6cce502d726b7cdf6cec4fc
Laura Oviedo <lauraoviedosocias@gmail.com>
2022-01-14 18:34:08 -0300

    Agreguo funciones basicas de navigacion

--- index.js
+++ index.js
@@ -5,14 +5,52 @@ const homeButton = document.getElementById("home-button")
 const menu_hamburguesa = document.getElementById("menu-hamburguesa")
 
 
-const charactersSection = document.getElementById("")
+const charactersSection = document.getElementById("characters-section")
+const episodesSection = document.getElementById("episodes-section")
+const quotesSection = document.getElementById("quotes-section")
+const homeSection = document.getElementById("home-section")
 
 // BASIC FUNCTIONS TO NAVIGATE THE WEB
 
 charactersButton.onclick = () => {
 
-    cards_principales.classList.add("is-hidden");
-    card_reportes.classList.add("is-hidden");
-    card_categorias.classList.remove("is-hidden");
+    homeSection.style.display = "none"
+    episodesSection.style.display = "none"
+    quotesSection.style.display = "none"
+    charactersSection.style.display = "flex"
+    
+}
+
+episodesButton.onclick = () => {
+    
+    homeSection.style.display = "none"
+    quotesSection.style.display = "none"
+    charactersSection.style.display = "none"
+    episodesSection.style.display = "flex"
 
 }
+
+quotesButton.onclick = () => {
+
+    homeSection.style.display = "none"
+    episodesSection.style.display = "none"
+    charactersSection.style.display = "none"    
+    quotesSection.style.display = "flex"
+
+}
+
+homeButton.onclick = () => {
+
+    episodesSection.style.display = "none"
+    charactersSection.style.display = "none"    
+    quotesSection.style.display = "none"
+    homeSection.style.display = "flex"
+}
+
+// API FUNTIONS
+
+const inputCharacters = document.getElementById("input-characters-search")
+const inputEpisodes = document.getElementById("input-episodes-search")
+const inputQuotes = document.getElementById("input-quotes-search")
+
+fetch
